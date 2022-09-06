6127f8ef91c12ab2d3007976f206724c915411d4
gopal2k6 <gopal2k6.1983@gmail.com>
2022-09-06 14:24:37 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/ExpenseEntryItemList';
+import ExpenseEntryItemList from './components/ExpenseEntryItemList';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);

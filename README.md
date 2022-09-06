2a2d6c500dbe77510d9e306bc72c4bdafbd7dc0f
gopal2k6 <gopal2k6.1983@gmail.com>
2022-09-06 14:23:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-p6e9p2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-p6e9p2)
---
+++ components/ExpenseEntryItemList.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+
+export default class ExpenseEntryItemList extends React.Component() {
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    );
+  }
+}

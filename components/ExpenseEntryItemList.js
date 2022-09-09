fa221f87745fd086e78183ddd269cbd02ae6b7ac
gopal2k6 <gopal2k6.1983@gmail.com>
2022-09-09 16:29:44 +0530

    1 file modified

--- components/ExpenseEntryItemList.js
+++ components/ExpenseEntryItemList.js
@@ -5,14 +5,27 @@ import '../style.css';
 export default class ExpenseEntryItemList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { info: 'test ok' };
   }
+
   render() {
+    //this.setState({ info: 'test ok1' });
+
+    this.setState((state, props) => ({
+      info: 'ok test 22222'
+    }));
+
     const lists = this.props.lists.map((item) => (
       <tr key={item.key}>
+        <td>{item.id}</td>
         <td>{item.name}</td>
         <td>{item.amount}</td>
         <td>{item.spendDate}</td>
         <td>{item.category}</td>
+        <td>
+          <a href="#">{this.state.info}</a>
+        </td>
       </tr>
     ));
 
@@ -21,10 +34,12 @@ export default class ExpenseEntryItemList extends React.Component {
         <table>
           <thead>
             <tr>
+              <th>ID</th>
               <th>Name</th>
               <th>Amount</th>
               <th>Date</th>
               <th>Category</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>{lists}</tbody>
